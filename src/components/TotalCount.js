484b79ae9d49fec0477e222cbbd929fcbf80c74f
keerthu1991 <keerthanambika03@gmail.com>
2021-02-18 13:41:42 +0530

    changed

--- src/components/TotalCount.js
+++ src/components/TotalCount.js
@@ -4,13 +4,13 @@ import Container from 'react-bootstrap/Container';
 
 export default function TotalCount(totalval,red,blue,green) {
         totalval= parseInt(red + blue + green);
-        console.log(parseInt(totalval));
+        console.log(parseInt(totalval));//printing the total click counts in console
     return (
         <div>
             <Container>
             <Button variant="primary">total</Button>
             </Container>
-            
+
             
         </div>
     )
